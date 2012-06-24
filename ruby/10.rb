549cb4df3e098bb732b46f413ae039779a85086c
John Hoo <huziyong@gmail.com>
2012-06-24 10:33:16 +0800

    refactor testing and optimize problem 10's solution algorithm

--- ruby/10.rb
+++ ruby/10.rb
@@ -1,5 +1,53 @@
 #coding: utf-8
 
+# The sieve of Eratosthenes
+
+class Prime
+  def initialize(max_num)
+    @max = max_num
+    @map = [true]*(max_num+1)
+    @map[0] = false
+    @map[1] = false
+    @pos = 2
+  end
+
+  def sieve_multiple!
+    i = 2
+    while @pos * i <= @max
+      @map[@pos * i] = false
+      i += 1
+    end
+  end
+
+  def sieve!
+    sqr = Math.sqrt(@max).round
+    while @pos <= sqr
+      sieve_multiple!
+      next_pos!
+    end
+  end
+  
+  def next_pos!
+    while @pos < @max
+      @pos += 1
+      return @pos if @map[@pos]
+    end
+  end
+
+  def primes
+    @primes = []
+    @map.each_with_index {|x, i| @primes << i if x }
+    @primes
+  end
+end
+
+p = Prime.new(2_000_000)
+p.sieve!
+puts p.primes.inject(0) {|memo, x| memo + x}
+
+
+#
+=begin
 def prime? num
   sqr = Math.sqrt(num).round
   2.upto(sqr) {|x| return false if num % x == 0}
@@ -14,8 +62,9 @@ while p < 2_000_000
 end
 
 puts sum
+=end
 
-
+# 
 =begin
 def remove(primes, factor, max)
   i = 2
--- test/test_all_rubies.rb
+++ test/test_all_rubies.rb
@@ -1,16 +1,18 @@
 #coding: utf-8
 
 require "test/unit"
+#require "benchmark"
 
 $root = ARGV[0]
 
-def right?(answer_path, ruby_path)
-  answer = File.open(answer_path, "r").readlines.last.strip
+def answer answer_path
+  File.open(answer_path, "r").readlines.last.strip
+end
+
+def result ruby_path
   results = `ruby #{ruby_path}`
-  result = results.strip.lines.to_a.last
-  #puts "a:[#{answer}], r:[#{result}]"
-  answer == result
-end 
+  results.strip.lines.to_a.last
+end
 
 class TestAll < Test::Unit::TestCase
 
@@ -22,15 +24,20 @@ rubies_path = $root + "/ruby"
 
 Dir.foreach(problems_path) do |problem|
   next if problem == "." or problem == ".."
+  
+  #puts "Solving problem #{problem}" 
+  p1 = Time.now
   answer_path = answers_path + "/" + problem
   ruby_path = rubies_path + "/" + problem + ".rb"
   if not File.exist? answer_path
     puts "answer for problem #{problem} is not exist"
   elsif not File.exist? ruby_path
     assert(false, "solution for problem #{problem} is not exist")
-  else 
-    assert(right?(answer_path, ruby_path))
+  else
+    assert_equal(answer(answer_path), result(ruby_path), "Solution of problem #{problem} is not equal to right answer.") 
   end
+  p2 = Time.now
+  puts "Solving problem #{problem} costs #{p2 - p1}"
 end
 
   end
